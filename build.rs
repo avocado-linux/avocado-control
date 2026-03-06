4301e0a979cbbc73b53b9200e493c351998f43ad
Justin Schneck <j.schneck@peridio.com>
2026-03-06 18:53:41 -0500

    fix: enable rustfmt in varlink code generator
    Passing true to cargo_build_tosource ensures generated .rs files are
    formatted consistently, preventing them from appearing as modified after
    every cargo build.

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
-    varlink_generator::cargo_build_tosource("src/varlink/org.avocado.Extensions.varlink", false);
-    varlink_generator::cargo_build_tosource("src/varlink/org.avocado.Runtimes.varlink", false);
-    varlink_generator::cargo_build_tosource("src/varlink/org.avocado.Hitl.varlink", false);
-    varlink_generator::cargo_build_tosource("src/varlink/org.avocado.RootAuthority.varlink", false);
+    varlink_generator::cargo_build_tosource("src/varlink/org.avocado.Extensions.varlink", true);
+    varlink_generator::cargo_build_tosource("src/varlink/org.avocado.Runtimes.varlink", true);
+    varlink_generator::cargo_build_tosource("src/varlink/org.avocado.Hitl.varlink", true);
+    varlink_generator::cargo_build_tosource("src/varlink/org.avocado.RootAuthority.varlink", true);
 }
